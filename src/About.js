5428e6aca17867432e5d877324e9e0568afcd78c
dreamyvoice <oopo0320@gmail.com>
2022-08-17 18:09:16 +0900

    1 file modified

--- src/About.js
+++ src/About.js
@@ -8,6 +8,65 @@ function About() {
 
         <span className="header2">08.09화 ~08.10 수 / 성인 1명, 일반석</span>
       </fieldset>
+
+      <fieldset>
+        <span>flight code</span>
+
+        <p>
+          <input
+            type="text"
+            name="airline"
+            placeholder="항공사를 입력하세요"
+          ></input>
+        </p>
+        <p>
+          <textarea
+            type="text"
+            name="
+            Starting point"
+            placeholder="출발지를 입력하세요"
+          ></textarea>
+        </p>
+        <p>
+          <textarea
+            type="text"
+            name="planning time"
+            placeholder="계획시간을 입력하세요"
+          ></textarea>
+        </p>
+        <p>
+          <textarea
+            type="text"
+            name="Estimated time"
+            placeholder="에상시간을 입력하세요"
+          ></textarea>
+        </p>
+        <p>
+          <textarea
+            type="text"
+            name="arrival"
+            placeholder="도착시간을 입력하세요"
+          ></textarea>
+        </p>
+        <p>
+          <textarea
+            type="text"
+            name="division"
+            placeholder="화물/승객을 입력하세요"
+          ></textarea>
+        </p>
+        <p>
+          <textarea
+            type="text"
+            name="Status"
+            placeholder="도착/지연을 입력하세요"
+          ></textarea>
+        </p>
+
+        <p>
+          <input type="submit" value="생성하기"></input>
+        </p>
+      </fieldset>
     </>
   );
 }
